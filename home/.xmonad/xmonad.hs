e6d84d0b698789084a36b00484242943bea1155a
Chris Lucas <chris@chriswlucas.com>
2019-01-02 08:06:37 -0800

    adds blueman applet launch

--- home/.xmonad/xmonad.hs
+++ home/.xmonad/xmonad.hs
@@ -59,6 +59,7 @@ myStartupHook = setWMName "LG3D"
 
     <+> spawnOnce "trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 10 --transparent true --tint 0x000000 --height 18 --alpha 0 --monitor primary --iconspacing 5 --padding 5"
     <+> spawnOnce "nm-applet --sm-disable" 
+    <+> spawnOnce "blueman-applet" 
     <+> spawnOnce "xscreensaver -no-splash" 
     <+> spawnOnce "xbanish" 
     <+> spawnOnce "jetbrains-toolbox --minimize" 
