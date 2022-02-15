d928d9a5e2ff2ccd7a8b40d4282968ebefd19c4d
Chris Lucas <chris@chriswlucas.com>
2022-02-14 19:12:30 -0800

    fixes?

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-set -x
 
 # Script idempotently installs dependencies and configures system
 # To be run *after* cloning and linking the repo
-
-# Setup i3
-/usr/lib/apt/apt-helper download-file https://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2020.02.03_all.deb keyring.deb SHA256:c5dd35231930e3c8d6a9d9539c846023fe1a08e4b073ef0d2833acd815d80d48
-sudo dpkg -i ./keyring.deb
-echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" | sudo tee /etc/apt/sources.list.d/sur5r-i3.list
-rm keyring.deb
 
 # Setup chrome
 wget -qO - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
@@ -24,6 +17,9 @@ sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
 rm packages.microsoft.gpg
 
+# Setup alacritty
+sudo add-apt-repository ppa:mmstick76/alacritty
+
 # System 76 repo
 sudo apt-add-repository -y ppa:system76-dev/stable
 
@@ -32,43 +28,49 @@ upgrade-all
 
 # Apt packages that can be auto installed
 sudo apt install \
-    arandr \
-    at \
+    alacritty \
     blueman \
+    brightnessctl \
     build-essential \
     clipit \
     code \
-    compton \
-    feh \
+    fonts-powerline \
     git \
     google-chrome-stable \
-    hsetroot \
     htop \
-    i3 \
     jq \
     libreoffice \
-    libxi-dev `# xbanish dependency` \
-    libxt-dev `# xbanish dependency` \
+    mlocate \
     moreutils \
     pavucontrol \
+    playerctl \
     policykit-1-gnome \
     py3status \
     python3-pydbus \
     rsync \
     rxvt-unicode-256color \
-    scrot \
+    slurp \
     spotify-client \
-    system76-driver \
     ssmtp \
+    sway \
+    swayidle \
+    swaylock \
+    system76-driver \
     tldr \
     tmux \
     tree \
     vim \
-    xbacklight \
-    xcape \
-    xclip \
-    xscreensaver \
+    wl-clipboard \
     zsh
+
+# interception tools dep
+sudo apt install
+    cmake \
+    libboost-all-dev \
+    libevdev-dev \
+    libsystemd-dev \
+    libudev-dev \
+    libyaml-cpp-dev
 
 # Install snaps
 sudo snap install \
@@ -91,16 +93,17 @@ mkdir -p "$HOME/workplace"
 mkdir -p "$HOME/.ssh"
 chmod 700 "$HOME/.ssh"
 
-# Set up udev keyboard rule
-cat << EOF | sudo tee /etc/udev/rules.d/99-usb-keyboard.rules > /dev/null
-ACTION=="add", SUBSYSTEM=="input", RUN+="/bin/su $USER -c '/bin/cat $HOME/bin/keyboard | /usr/bin/at now'"
+# gives permission to change brightness
+sudo usermod -aG video "$USER"
+
+# configure udevmon jobs
+cat <<'EOF' | sudo tee /etc/interception/udevmon.yaml > /dev/null
+- JOB: intercept -g $DEVNODE | caps2esc -m 1 | uinput -d $DEVNODE
+  DEVICE:
+    EVENTS:
+      EV_KEY: [KEY_CAPSLOCK, KEY_ESC]
 EOF
-sudo udevadm control --reload
-udevadm trigger
+
 
 echo "\nThe following must be installed/configured manually:"
-echo "    ssh keys"
-command -v playerctl            || echo "    playerctl           https://github.com/acrisci/playerctl/releases"
-command -v xbanish              || echo "    xbanish             https://github.com/jcs/xbanish/releases"
-fc-list | grep -qi "powerline"  || echo "    powerline-fonts     https://github.com/powerline/fonts"
-
+command -v caps2esc >/dev/null || echo "    caps2esc            https://gitlab.com/interception/linux/plugins/caps2esc"
